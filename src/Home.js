5ed969bd4d65da748ce08e3a33e6b25b5e58549a
amogh <amoghtripathi@gmail.com>
2020-08-23 15:49:02 +0530

    added product component for the produxts

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Product from "./Product";
 import "./Home.css";
 
 function Home() {
@@ -7,6 +8,17 @@ function Home() {
             <img className="home__image" 
             src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
             alt="bannerIMG" />
+
+        {/* Producr with id name price star image */}
+
+        <Product
+            id="123"
+            title="the book: how to read and write"
+            price={11}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg"
+            
+        />
         </div>
     )
 }
