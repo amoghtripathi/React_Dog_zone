16335379099e976866cbeef4c7e5f45186888621
amogh <amoghtripathi@gmail.com>
2020-08-27 17:50:00 +0530

    home page basic skeleton with product component

--- src/Home.js
+++ src/Home.js
@@ -10,15 +10,60 @@ function Home() {
             alt="bannerIMG" />
 
         {/* Producr with id name price star image */}
-
+        <div className="home__row">
+            <Product
+                id="123"
+                title="The Book: How to read and write"
+                price={11}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg"
+                
+            />
+            <Product
+                id="123"
+                title="The Book: How to read and write"
+                price={11}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg"
+                
+            />
+        </div>
+        <div className="home__row">
         <Product
-            id="123"
-            title="The Book: How to read and write"
-            price={11}
-            rating={5}
-            image="https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg"
-            
-        />
+                id="123"
+                title="The Book: How to read and write"
+                price={11}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg"
+                
+            />
+            <Product
+                id="123"
+                title="The Book: How to read and write"
+                price={11}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg"
+                
+            />
+            <Product
+                id="123"
+                title="The Book: How to read and write"
+                price={11}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg"
+                
+            />
+        </div>
+        <div className="home__row">
+            <Product
+                    id="123"
+                    title="The Book: How to read and write"
+                    price={11}
+                    rating={5}
+                    image="https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg"
+                    
+                />
+        </div>
         </div>
     )
 }
