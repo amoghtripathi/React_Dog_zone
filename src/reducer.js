efc2ef0e7ad07eba3fd13d4dfd0299906f8c1220
amogh <amoghtripathi@gmail.com>
2020-08-28 03:21:54 +0530

    react-context-api part 2 (complete) and added unique dummy data

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,16 @@
 export const initialState ={
     basket: [],
+    user: null,
 };
 
 const reducer=(state, action)=>{
-    console.log(action);
+    // console.log(action); 
     switch(action.type){
         case 'ADD_TO_BASKET':
-            return {state};
+            return {
+                ...state,
+                basket: [...state.basket, action.item],
+            };
             
         case 'REMOVE_FROM_BASKET':
             return {state};
