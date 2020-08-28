2cc2b6d9d3b8f22366509b7c1bc8382e5f81b249
amogh <amoghtripathi@gmail.com>
2020-08-29 02:02:00 +0530

    added subtotal component on checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { useStateValue } from './StateProvider';
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from './Subtotal';
 
 function Checkout() {
     const [{ basket }]=useStateValue();
 
     return (
         <div className="checkout">
+            <div className="checkout_left">
             <img className="checkout__ad"
             src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
             alt=""
@@ -21,8 +23,8 @@ function Checkout() {
                 </p>
             </div>
         ): (
-            <div className="checkout__title">
-                <h2>Your Shopping Basket: </h2>
+            <div>
+                <h2 className="checkout__title">Your Shopping Basket: </h2>
                 
                 {/* CheckOut Products */}
 
@@ -35,12 +37,17 @@ function Checkout() {
                         rating={item.rating}
                     />
                 ))}
-
+            </div>
+            )}
+        </div>
+        {basket.length > 0 && (
+            <div className="checkout__right">
+                
+              <Subtotal />
             </div>
         )}
-
-        </div>
-    )
+    </div>
+    );
 }
 
 export default Checkout;
